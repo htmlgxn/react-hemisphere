64912b230eb13f99a1655b99c0ced2a793d2d055
Ben Chitty <htmlgxn@protonmail.com>
2021-05-08 17:55:28 -0300

    Learned JS geolocation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            latitude: null,
+            longitude: null,
+            errorMessage: ''
+        }
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    latitude: position.coords.latitude, 
+                    longitude: position.coords.longitude
+                })
+            },
+            (error) => {
+                this.setState({
+                    errorMessage: error.message
+                })
+            }
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    {this.state.latitude}
+                    {this.state.errorMessage}
+                </div>
+                <div>
+                    {this.state.longitude}
+                </div>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector('#root')
+)
